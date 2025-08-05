db06703c4afc7257207a7f66ff8c2888908175cc
Stephen-Beck <stephenbeck89@outlook.com>
2025-08-05 17:00:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write a program in C# Sharp to print numbers from n to 1 using recursion.
+
+    Test Data :
+        How many numbers to print : 10
+        Expected Output :
+            10 9 8 7 6 5 4 3 2 1 
+*/
+
+namespace Assignment_5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 0;
+            while (true)
+            {
+                Console.Write("How many numbers would you like to print? ");
+                if (!int.TryParse(Console.ReadLine(), out nums) || nums < 1)
+                    Console.Write("Invalid number. ");
+                else break;
+            }
+            PrintNumber(nums);
+        }
+
+        static void PrintNumber(int n)
+        {
+            Console.Write(n + " ");
+
+            if (n > 1)
+                PrintNumber(n - 1);
+        }
+    }
+}
